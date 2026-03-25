c7fb495daca87477b8c548c7c3ab1d78cd062eb3
Harshita2020 <harshitaautocad@gmail.com>
2026-03-25 23:57:54 +0530

    Feat: adding sorting in View Attendance.

--- client/src/components/ViewAttendance.jsx
+++ client/src/components/ViewAttendance.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const ViewAttendance = ({attendance}) => {
-    console.log("Att data- ", attendance)
+const ViewAttendance = ({ attendance }) => {
+  console.log("Att data- ", attendance);
+  const filteredAttendanceByDate = attendance.length > 0 && attendance.reduce((acc, record) => {
+    const date = new Date(record.date).toLocaleDateString() // Format date to a readable format
+    if (!acc[date]) {
+      acc[date] = [];
+    } else {
+      acc[date].push(record);
+    } 
+    return acc;
+  }, {});
+  console.log("FILTERED!- ", filteredAttendanceByDate)
   return (
     <>
-    <table className="min-w-full bg-white border border-gray-200">
-      <thead>   
-        <tr>
+      {filteredAttendanceByDate && filteredAttendanceByDate.length > 0 ? 
+      <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
+        <thead>
+          <tr>
             <th className="py-2 px-4 border-b">Employee ID</th>
             <th className="py-2 px-4 border-b">Date</th>
             <th className="py-2 px-4 border-b">Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {attendance.map((record) => (
-            <tr key={record._id}>   
-                <td className="py-2 px-4 border-b">{record.employeeId}</td>
-                <td className="py-2 px-4 border-b">{new Date(record.date).toLocaleDateString()}</td>
-                <td className="py-2 px-4 border-b">{record.status}</td>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredAttendanceByDate.map((record) => (
+            <tr key={record._id}>
+              <td className="py-2 px-4 border-b">{record.employeeId}</td>
+              <td className="py-2 px-4 border-b">
+                {new Date(record.date).toLocaleDateString()}
+              </td>
+              <td className="py-2 px-4 border-b">{record.status}</td>
             </tr>
-        ))} 
-      </tbody>
-      </table>
+          ))}
+        </tbody>
+      </table> : 
+       <p className="text-center text-gray-500">No attendance records found.</p>}
     </>
-  )
-}
+  );
+};
 
-export default ViewAttendance
+export default ViewAttendance;
